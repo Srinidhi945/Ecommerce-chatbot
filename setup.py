78f1ff949ab378d0f7c972042bdf8b0c097dc295
srinidhi945 <srinidhiporeddy@gmail.com>
2025-08-05 21:42:28 +0530

    commit for e-commerce chatbot

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="Ecommercebot",
     version="0.0.1",
-    author="irshad",
-    author_email="miz.irs2@gmail.com",
+    author="Srinidhi Poreddy",
+    author_email="srinidhiporeddy@gmail.com",
     packages=find_packages(),
     install_requires=['langchain-astradb','langchain ','langchain-openai','datasets','pypdf','python-dotenv','flask']
 )
